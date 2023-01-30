ebd08099a9a202414a4235d659ede87368eaca79
Petr Smejkal <xmejkal@gmail.com>
2023-01-30 12:42:18 +0100

    added untested flat and photo data model definitions

--- sreality_flask.py
+++ sreality_flask.py
@@ -6,6 +6,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sreality.db"
 db = SQLAlchemy(app)
 
 
+class Flat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    photos = db.relationship("Photo", backref="flat", lazy=True)
+
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(80), unique=True, nullable=False)
+    flat_id = db.Column(db.Integer, db.ForeignKey("flat.id"), nullable=False)
+    flat = db.relationship("Flat", backref=db.backref("photos", lazy=True))
+
+
 @app.route("/")
 def hello():
     return render_template("index.html", content="Hello World!")
